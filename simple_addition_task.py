e266d815a0b8544b54168370d09639411db3677f
Mahmoud mohamed <143263824+7oda-dev@users.noreply.github.com>
2024-11-14 18:25:20 +0200

    Update simple_addition_task.py

--- simple_addition_task.py
+++ simple_addition_task.py
@@ -1,4 +1,6 @@
 def add(a, b):
      if not all(isinstance(i, (int, float)) for i in (a, b)):
-        raise TypeError("Both arguments must be numbers.")
-    return a + b
+        raise TypeError("Both arguments must be numbers."
+ return a+b                       
+                        
+                        
